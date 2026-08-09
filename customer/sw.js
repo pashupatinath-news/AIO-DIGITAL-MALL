ef7810e569402399754f6259b799103384dd6c15
pashupatinath-news <aklove012016@gmail.com>
2026-08-09 20:39:22 +0530

    Update sw.js

--- customer/sw.js
+++ customer/sw.js
@@ -1,7 +1,6 @@
-const CACHE_NAME = "aio-digital-mall-customer-v1";
+const CACHE_NAME = "aio-customer-v1";
 
-const APP_FILES = [
-  "./",
+const FILES = [
   "./index.html",
   "./app.js",
   "./manifest.json"
@@ -11,12 +10,11 @@ self.addEventListener("install", event => {
 
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache =>
-        cache.addAll(APP_FILES)
-      )
+      .then(cache => cache.addAll(FILES))
   );
 
   self.skipWaiting();
+
 });
 
 self.addEventListener("activate", event => {
@@ -25,61 +23,23 @@ self.addEventListener("activate", event => {
     caches.keys().then(keys =>
       Promise.all(
         keys
-          .filter(key =>
-            key !== CACHE_NAME
-          )
-          .map(key =>
-            caches.delete(key)
-          )
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
       )
     )
   );
 
   self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
 
-  if (
-    event.request.method !== "GET"
-  ) {
-    return;
-  }
-
   event.respondWith(
-
     caches.match(event.request)
-      .then(cached => {
-
-        if (cached) {
-          return cached;
-        }
-
-        return fetch(event.request)
-          .then(response => {
-
-            if (
-              response &&
-              response.status === 200 &&
-              response.type === "basic"
-            ) {
-
-              const clone =
-                response.clone();
-
-              caches.open(CACHE_NAME)
-                .then(cache =>
-                  cache.put(
-                    event.request,
-                    clone
-                  )
-                );
-            }
-
-            return response;
-
-          });
-
-      })
+      .then(response =>
+        response || fetch(event.request)
+      )
   );
+
 });
