c431ab2a25895ce1c4ec36364f8fbec138eb5d61
pashupatinath-news <aklove012016@gmail.com>
2026-08-09 20:45:36 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,45 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('v11').then(c => c.addAll(['index.html','app.js'])));
+const CACHE_NAME = "aio-customer-v1";
+
+const FILES = [
+  "./index.html",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
+
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
+
+  self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response =>
+        response || fetch(event.request)
+      )
+  );
+
 });
