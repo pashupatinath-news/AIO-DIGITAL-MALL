0f2a4a9823c147b12ebccf34c793d658e40d25e6
pashupatinath-news <aklove012016@gmail.com>
2026-08-09 21:02:08 +0530

    Create firebaseConfig.js

---
+++ team/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAyH9RRJTEETFh4zHWwCMO4d6qMielJQFA",
+  authDomain: "aio-digital-mall.firebaseapp.com",
+  projectId: "aio-digital-mall",
+  storageBucket: "aio-digital-mall.firebasestorage.app",
+  messagingSenderId: "501384049673",
+  appId: "1:501384049673:web:968bd8311cc700f82874d8",
+  measurementId: "G-TNDT9FYNRP"
+};
