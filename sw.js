6c69edcbb4251d4d793fe496f2e421a3351cccfb
pashupatinath-news <aklove012016@gmail.com>
2026-08-09 20:57:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,73 @@
-const CACHE_NAME = "aio-customer-v1";
+const CACHE_NAME =
+  "aio-digital-mall-v1";
 
 const FILES = [
+  "./",
   "./index.html",
   "./app.js",
+  "./firebaseConfig.js",
   "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
 
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES))
-  );
+self.addEventListener(
+  "install",
+  event => {
 
-  self.skipWaiting();
+    event.waitUntil(
 
-});
+      caches.open(
+        CACHE_NAME
+      ).then(cache =>
 
-self.addEventListener("activate", event => {
+        cache.addAll(
+          FILES
+        )
 
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(k => k !== CACHE_NAME)
-          .map(k => caches.delete(k))
       )
-    )
-  );
 
-  self.clients.claim();
+    );
 
-});
+    self.skipWaiting();
 
-self.addEventListener("fetch", event => {
+  }
+);
 
-  event.respondWith(
-    caches.match(event.request)
-      .then(response =>
-        response || fetch(event.request)
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+      self.clients.claim()
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    if (
+      event.request.method !==
+      "GET"
+    ) {
+      return;
+    }
+
+    event.respondWith(
+
+      caches.match(
+        event.request
+      ).then(
+        cached =>
+          cached ||
+          fetch(event.request)
       )
-  );
 
-});
+    );
+
+  }
+);
