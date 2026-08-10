af16db2102635374e268fdeafed1e681621e6c49
pashupatinath-news <aklove012016@gmail.com>
2026-08-10 11:49:20 +0530

    Update firebaseConfig.js

--- customer/firebaseConfig.js
+++ customer/firebaseConfig.js
@@ -1,9 +1,4 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAyH9RRJTEETFh4zHWwCMO4d6qMielJQFA",
-  authDomain: "aio-digital-mall.firebaseapp.com",
-  projectId: "aio-digital-mall",
-  storageBucket: "aio-digital-mall.firebasestorage.app",
-  messagingSenderId: "501384049673",
-  appId: "1:501384049673:web:968bd8311cc700f82874d8",
-  measurementId: "G-TNDT9FYNRP"
-};
+export {
+  auth,
+  db
+} from "../firebaseConfig.js";
