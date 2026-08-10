161eca6439d80d0a73f56e84fdfff1216ed8233c
pashupatinath-news <aklove012016@gmail.com>
2026-08-10 11:52:30 +0530

    Update firebaseConfig.js

--- team/firebaseConfig.js
+++ team/firebaseConfig.js
@@ -1,9 +1,46 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAyH9RRJTEETFh4zHWwCMO4d6qMielJQFA",
-  authDomain: "aio-digital-mall.firebaseapp.com",
-  projectId: "aio-digital-mall",
-  storageBucket: "aio-digital-mall.firebasestorage.app",
-  messagingSenderId: "501384049673",
-  appId: "1:501384049673:web:968bd8311cc700f82874d8",
-  measurementId: "G-TNDT9FYNRP"
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+
+const firebaseConfig = {
+
+  apiKey:
+    "AIzaSyAyH9RRJTEETFh4zHWwCMO4d6qMielJQFA",
+
+  authDomain:
+    "aio-digital-mall.firebaseapp.com",
+
+  projectId:
+    "aio-digital-mall",
+
+  storageBucket:
+    "aio-digital-mall.firebasestorage.app",
+
+  messagingSenderId:
+    "501384049673",
+
+  appId:
+    "1:501384049673:web:968bd8311cc700f82874d8",
+
+  measurementId:
+    "G-TNDT9FYNRP"
 };
+
+const app =
+  initializeApp(firebaseConfig);
+
+export const auth =
+  getAuth(app);
+
+export const db =
+  getFirestore(app);
+
+export default app;
