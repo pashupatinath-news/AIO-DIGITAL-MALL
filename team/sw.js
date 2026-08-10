743b6d2213f409a63b7a6ddfdd72e44e633dd80d
pashupatinath-news <aklove012016@gmail.com>
2026-08-10 11:53:17 +0530

    Update sw.js

--- team/sw.js
+++ team/sw.js
@@ -11,62 +11,86 @@ const FILES = [
   "./manifest.json"
 ];
 
-
 self.addEventListener(
   "install",
   event => {
 
     event.waitUntil(
 
-      caches.open(
-        CACHE_NAME
-      ).then(
-        cache =>
-          cache.addAll(
-            FILES
-          )
-      )
+      caches
+        .open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(FILES)
+        )
+        .then(() =>
+          self.skipWaiting()
+        )
 
     );
 
-    self.skipWaiting();
-
   }
 );
-
 
 self.addEventListener(
   "activate",
   event => {
 
     event.waitUntil(
-      self.clients.claim()
+
+      caches.keys().then(keys =>
+        Promise.all(
+          keys
+            .filter(
+              key =>
+                key !== CACHE_NAME
+            )
+            .map(key =>
+              caches.delete(key)
+            )
+        )
+      ).then(() =>
+        self.clients.claim()
+      )
+
     );
 
   }
 );
-
 
 self.addEventListener(
   "fetch",
   event => {
 
     if (
-      event.request.method !==
-      "GET"
+      event.request.method !== "GET"
     ) {
       return;
     }
 
     event.respondWith(
 
-      caches.match(
-        event.request
-      ).then(
-        cached =>
-          cached ||
-          fetch(event.request)
-      )
+      fetch(event.request)
+        .then(response => {
+
+          const copy =
+            response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache =>
+              cache.put(
+                event.request,
+                copy
+              )
+            );
+
+          return response;
+
+        })
+        .catch(() =>
+          caches.match(
+            event.request
+          )
+        )
 
     );
 
